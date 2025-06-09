3d1cbfc66ca6e78c6fb09e207b45c85a78d1bb80
Olesia Bulatova <syasa80@mail.ru>
2025-06-09 20:33:39 +0400

    Second commit

--- methods/order_methods.py
+++ methods/order_methods.py
@@ -1,26 +1,31 @@
 import requests
 from data import *
+import allure
 
 
 class OrderMethods:
 
     @staticmethod
+    @allure.step("Создаем заказ")
     def create_order(ingredients, access_token=''):
         response = requests.post(f"{Urls.BASE_URL}{Urls.CREATE_ORDER_URL}", data=ingredients,
                                  headers={'Authorization': access_token})
         return response
 
     @staticmethod
+    @allure.step("Получение заказов конкретного авторизованного пользователя")
     def get_current_user_orders_with_authorization(access_token):
         response = requests.get(f"{Urls.BASE_URL}{Urls.GET_USER_ORDERS_URL}", headers={'Authorization': access_token})
         return response
 
     @staticmethod
+    @allure.step("Получение заказов конкретного пользователя без авторизации")
     def get_current_user_orders_without_authorization():
         response = requests.get(f"{Urls.BASE_URL}{Urls.GET_USER_ORDERS_URL}")
         return response
 
     @staticmethod
+    @allure.step("Получение списка ингредиентов")
     def get_ingredients_list():
         response = requests.get(f"{Urls.BASE_URL}{Urls.INGREDIENTS_LIST_URL}")
         return response
--- methods/users_methods.py
+++ methods/users_methods.py
@@ -1,23 +1,24 @@
+import allure
 import requests
 from data import *
 
 
 class UserMethods:
 
-    # Создаем уникального пользователя
     @staticmethod
+    @allure.step("Создаём уникального пользователя")
     def create_unique_user(payload):
         response = requests.post(f"{Urls.BASE_URL}{Urls.USER_CREATE_URL}", payload)
         return response
 
-    # Авторизуемся с логином и паролем
     @staticmethod
+    @allure.step("Авторизуемся с логином и паролем")
     def login_user(payload):
         response = requests.post(f"{Urls.BASE_URL}{Urls.USER_LOGIN_URL}", payload)
         return response
 
-    # Изменяем данные пользователя
     @staticmethod
+    @allure.step("Меняем данные пользователя")
     def change_user_data(access_token, payload):
         response = requests.patch(f"{Urls.BASE_URL}{Urls.USER_DATA_UPDATE_URL}",
                                   headers={'Authorization': access_token}, data=payload)
